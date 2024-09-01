c8f1580507bf98b3d648029f7bca9b0ab55f4e91
Ivan Somov <msijoc@gmail.com>
2024-09-01 22:27:28 +0300

    Created TalesAr Application

---
+++ app/src/main/cpp/TalesArApp.cpp
@@ -0,0 +1,12 @@
+#include "TalesArApp.hpp"
+
+namespace talesar {
+
+    TalesArApp::TalesArApp(AAssetManager *pAssetManager) : mAssetManager{pAssetManager}{
+
+    }
+
+    TalesArApp::~TalesArApp() {
+
+    }
+}
